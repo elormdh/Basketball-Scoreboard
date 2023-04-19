93c393548aa48ba93baea89a40a99bb46a0c82ba
elormdh <98886455+elormdh@users.noreply.github.com>
2023-04-19 19:43:26 +0000

    Add files via upload
    scoreboard app uploaded

---
+++ index.js
@@ -0,0 +1,36 @@
+let homeScore = document.getElementById("home-score")
+let guestScore = document.getElementById("guest-score")
+
+let homeScoreCounter = 0
+let guestScoreCounter = 0 
+
+function homeAdd1(){
+    homeScoreCounter = homeScoreCounter + 1
+    homeScore.textContent = homeScoreCounter
+}
+
+function homeAdd2(){
+    homeScoreCounter = homeScoreCounter + 2
+    homeScore.textContent = homeScoreCounter
+}
+
+function homeAdd3(){
+    homeScoreCounter = homeScoreCounter + 3
+    homeScore.textContent = homeScoreCounter
+}
+
+function guestAdd1(){
+    guestScoreCounter = guestScoreCounter + 1
+    guestScore.textContent = guestScoreCounter
+}
+
+function guestAdd2(){
+    guestScoreCounter = guestScoreCounter + 2
+    guestScore.textContent = guestScoreCounter
+}
+
+function guestAdd3(){
+    guestScoreCounter = guestScoreCounter + 3
+    guestScore.textContent = guestScoreCounter
+}
+
